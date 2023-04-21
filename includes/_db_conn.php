247d503be1d35281f4092a8b3679cdf2f9631763
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2023-04-21 13:52:31 +0530

    Manage User Added

--- includes/_db_conn.php
+++ includes/_db_conn.php
@@ -1,21 +1,19 @@
 <?php
-
 function sql_conn(){
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$db = "bit_leave_management_system";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
   echo "Connection Failed";
   die("Connection failed: " . $conn->connect_error);
 }
-
 return $conn;
 
 }
---
+++ utils/ManageUserUtils.php
@@ -0,0 +1,4 @@
+<?php
+include ('../../includes/_db_conn.php');
+$conn = sql_conn();
+?>

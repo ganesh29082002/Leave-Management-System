88e14c96496f59ca96f1dcbb9fd1b04775b0e8b5
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2023-04-21 13:16:18 +0530

    Navbar Created

--- pages/SuperAdmin/super_admin_dashboard.php
+++ pages/SuperAdmin/super_admin_dashboard.php
@@ -1,5 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Staff/StaffHomePage.css">
+    <link rel="stylesheet" href="../../includes/StaffNavBar.css?v=<?php echo time();?>">
+    <script src="https://kit.fontawesome.com/65712a75e6.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
 <?php 
-
-echo "Nice"
-
+include "../../includes/StaffNavBar.php";
 ?>
+   <section class="home-section">
+   <div class="horizontal_navbar">
+     <h1 class="Heading_Heder"> Bajaj Institute Technology Wardha</h1> 
+     
+     <a href="#">
+     <i class="fas fa-id-card-alt"></i>
+     <!-- <span class="tooltip">User</span> -->
+         
+        </a>
+     
+   </div>
+
+      <div class="text">Dashboard</div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est eligendi officiis at nemo eum deleniti natus, incidunt perspiciatis. Ipsum quos dolorum sint optio maiores ratione recusandae ea explicabo nam dignissimos.
+      </div>
+  </section>
+ 
+</body>
+</html>

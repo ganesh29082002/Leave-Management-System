1b7c7ec9ba61bb66f62ef58e9539c8fbaeb56f15
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-05-28 10:09:43 +0530

    Deactivated fucntionalities

--- utils/delete.php
+++ utils/delete.php
@@ -4,7 +4,9 @@ include ('../includes/_db_conn.php');
 $conn = sql_conn();
 
 $email = $_GET['email'];
-$query = "UPDATE user SET status='DEACTIVATED' WHERE email='$email'";
+$date = date('Y-m-d');
+
+$query = "UPDATE user SET status='DEACTIVATED' , deactivationDate='$date' WHERE email='$email'";
 $result = mysqli_query($conn, $query);
 if(isset($result)){
     header("Location: http://localhost/Leave-Management-System/pages/SuperAdmin/manageEmployees.php");
